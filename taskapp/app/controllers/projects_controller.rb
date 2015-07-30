94799e7b25193b72172d27d1b0679677e019e905
mhz <vitam.regit.fortuna@gmail.com>
2015-07-30 19:59:21 +0900

    DB edit forms ( the form is not communized )

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -12,13 +12,26 @@ class ProjectsController < ApplicationController
 	 @project = Project.new
 	end
 
+	def edit
+	 @project =Project.find(params[:id])
+	end
+
+	def update
+			@project =Project.find(params[:id])
+			if @project.update(project_params)
+				redirect_to projects_path
+			else
+				render 'edit'
+			end
+	end
+
 	def create
 	 @project = Project.new(project_params)
-	if @project.save
-		redirect_to projects_path
-	else
-		render 'new'
-	end
+		if @project.save
+			redirect_to projects_path
+		else
+			render 'new'
+		end
 	end
 
 private
--- taskapp/app/models/project.rb
+++ taskapp/app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
-	validates :title, presence: true
+	validates :title,
+			presence: {	message: "入力してください"},
+			length:   {	minimum: 3,  too_short: "4文字以上にしてください",
+									maximum: 10, too_long: "11文字以下にしてください"}
 end
